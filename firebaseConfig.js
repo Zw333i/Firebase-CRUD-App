73e9f0257ddfdcd8e859300ade0e9dc3fc4bcb4a
Zw333i <zjuztine@gmail.com>
2025-03-22 14:56:58 +0800

    Second Commit

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBYQ5vNKu8j0tPioD7w8aITq_diMo4HQlE",
-    authDomain: "mycrudapp-8e3c3.firebaseapp.com",
-    projectId: "mycrudapp-8e3c3",
-    storageBucket: "mycrudapp-8e3c3.firebasestorage.app",
-    messagingSenderId: "540214364727",
-    appId: "1:540214364727:web:aeabedf964f85bb5c54db2",
-    measurementId: "G-NLPXKK3QKF"
+
 };
 
 // Initialize Firebase
